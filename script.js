b05b40dcc762b527865b17aafd7ebaf08f0bea0f
FWFL <cod2camp@gmail.com>
2024-02-10 09:55:07 +0100

    ninho premier jour~

---
+++ script.js
@@ -0,0 +1,23 @@
+// $(function(){
+//    
+//    var video=["video/Arme de poing.mp4","video/bon qu'a ca.mp4","video/bad.mp4","video/No Love.mp4","video/jefe.mp4","video/verite.mp4"]
+//    $(".pannel").click(function(){
+//     var index = $(this).data(),
+//     src=video[index];
+//     $(".extrait")[index].fadeIut()
+//     $("video").attr("src",src)
+//     alert("lol")
+//    })
+// })
+$(function() {
+    var video = ["video/Arme de poing.mp4", "video/bon qu'a ca.mp4", "video/bad.mp4", "video/No Love.mp4", "video/jefe.mp4", "video/verite.mp4"];
+    $(".extrait").fadeOut()
+    $(".pannel").click(function() {
+       var x = $(this).index();
+       console.log(x)
+       var src = video[x];
+       $("video").attr("src", src);
+       $(".extrait").fadeOut(2000)
+        $(this).find(".extrait").fadeIn(3000);
+    });
+ });
